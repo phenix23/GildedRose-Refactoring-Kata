1c72b91344472cd72e1189eb831919c37a548513
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-15 19:44:57 +0100

    define accessors for attributes

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,26 @@ public class Item {
         this.quality = quality;
     }
 
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
