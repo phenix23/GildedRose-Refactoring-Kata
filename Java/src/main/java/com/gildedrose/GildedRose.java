42c4747e58c96187acb1d59eb025fac9f9584fa4
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-15 19:45:11 +0100

    refactoring updateQuality method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,56 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for(Item item : this.getItems()){
+            calculateQuality(item);
+            decrementSellIn(item);
+            if ( item.getSellIn() < 0) {
+                reCalculateQuality(item);
+            }
+        }
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+
+    private void calculateQuality(Item item) {
+        if ("Aged Brie".equals(item.getName()) ||"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            incrementQuality(item);
+        } else {
+            decrementQuality(item);
+        }
+    }
+
+    private void reCalculateQuality(Item item) {
+        if ("Aged Brie".equals(item.getName()) ) {
+            incrementQuality(item);
+        } else {
+            if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                item.setQuality(0);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                decrementQuality(item);
             }
+        }
+    }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    private void decrementSellIn(Item item) {
+         item.setSellIn("Sulfuras, Hand of Ragnaros".equals(item.getName()) ? item.getSellIn(): item.getSellIn() - 1);
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+    private void incrementQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+
+            if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()) && (item.getSellIn() < 11)) {
+                item.setQuality(item.getQuality() + 1);
             }
+        }
+    }
+
+    private void decrementQuality(Item item) {
+        if (item.getQuality() > 0 && !"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            item.setQuality(item.getQuality() - 1);
         }
     }
 }
