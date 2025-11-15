e4d6739d521c9f5147fcb5108dc3424b98e8aa50
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-15 19:45:04 +0100

    define parameterized Tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,38 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @ParameterizedTest
+    @MethodSource("updateQualityProvider")
+    void testUpdateQuality(String name , int sellIn , int quality, int expectedQuality) {
+        Item[] items = new Item[] { new Item(name,sellIn,quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(expectedQuality,app.getItems()[0].getQuality());
     }
 
+    private static Stream<Arguments> updateQualityProvider() {
+        return Stream.of(
+            Arguments.of(null, 0, 1, 0),
+            Arguments.of("foo", 0, 1, 0),
+            Arguments.of("foo", 0, 2, 0),
+            Arguments.of("foo", 0, -1, -1),
+            Arguments.of("Aged Brie", 0, 0, 2),
+            Arguments.of("Aged Brie", 1, 0, 1),
+            Arguments.of("Aged Brie", 0, 50, 50),
+            Arguments.of("Sulfuras, Hand of Ragnaros", 0, 1, 1),
+            Arguments.of("Sulfuras, Hand of Ragnaros", -1, 1, 1),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 0, 2, 0),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 11, 2, 3),
+            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 10, 2, 4)
+        );
+    }
 }
