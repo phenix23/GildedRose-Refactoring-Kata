b93c19bbc4d72b2cefe7df15a0670f332ed9270c
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-16 15:58:58 +0100

    delegate updateQuality to class QualityProcessor.java

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,43 +21,4 @@ class GildedRose {
         return items;
     }
 
-    private void calculateQuality(Item item) {
-        if ("Aged Brie".equals(item.getName()) ||"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-            incrementQuality(item);
-        } else {
-            decrementQuality(item);
-        }
-    }
-
-    private void reCalculateQuality(Item item) {
-        if ("Aged Brie".equals(item.getName()) ) {
-            incrementQuality(item);
-        } else {
-            if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                item.setQuality(0);
-            } else {
-                decrementQuality(item);
-            }
-        }
-    }
-
-    private void decrementSellIn(Item item) {
-         item.setSellIn("Sulfuras, Hand of Ragnaros".equals(item.getName()) ? item.getSellIn(): item.getSellIn() - 1);
-    }
-
-    private void incrementQuality(Item item) {
-        if (item.getQuality() < 50) {
-            item.setQuality(item.getQuality() + 1);
-
-            if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()) && (item.getSellIn() < 11)) {
-                item.setQuality(item.getQuality() + 1);
-            }
-        }
-    }
-
-    private void decrementQuality(Item item) {
-        if (item.getQuality() > 0 && !"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
-            item.setQuality(item.getQuality() - 1);
-        }
-    }
 }
