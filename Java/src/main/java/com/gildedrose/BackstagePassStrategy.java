c9ffb01cbfdfa1656ee3c69212b7c0450f883924
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-18 11:54:09 +0100

    fix updateQuality for values > 50

--- Java/src/main/java/com/gildedrose/BackstagePassStrategy.java
+++ Java/src/main/java/com/gildedrose/BackstagePassStrategy.java
@@ -5,12 +5,12 @@ public class BackstagePassStrategy implements ItemQualityStrategy {
     public void updateQuality(Item item) {
         if (item.getQuality() < 50) {
             item.setQuality(item.getQuality() + 1);
-        }
-        if ((item.getSellIn() < 11)) {
-            item.setQuality(item.getQuality() + 1);
-        }
-        if ((item.getSellIn() < 6)) {
-            item.setQuality(item.getQuality() + 1);
+            if ((item.getSellIn() < 11)) {
+                item.setQuality(item.getQuality() + 1);
+            }
+            if ((item.getSellIn() < 6)) {
+                item.setQuality(item.getQuality() + 1);
+            }
         }
         if (item.getSellIn() < 0) {
             item.setQuality(0);
