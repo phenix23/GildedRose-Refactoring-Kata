45360346950eb6665aedc7139251d68b8a2f47fd
pheonix_23 <hedahdia.faycel@gmail.com>
2025-11-20 12:16:43 +0100

    define Constants

--- Java/src/main/java/com/gildedrose/BackstagePassStrategy.java
+++ Java/src/main/java/com/gildedrose/BackstagePassStrategy.java
@@ -1,14 +1,19 @@
 package com.gildedrose;
 
 public class BackstagePassStrategy implements ItemQualityStrategy {
+
+    public static final int FIFTY_DAYS = 50;
+    public static final int ELEVEN_DAYS = 11;
+    public static final int SIX_DAYS = 6;
+
     @Override
     public void updateQuality(Item item) {
         if (item.getQuality() < 50) {
             item.setQuality(item.getQuality() + 1);
-            if ((item.getSellIn() < 11)) {
+            if ((item.getSellIn() < ELEVEN_DAYS)) {
                 item.setQuality(item.getQuality() + 1);
             }
-            if ((item.getSellIn() < 6)) {
+            if ((item.getSellIn() < SIX_DAYS)) {
                 item.setQuality(item.getQuality() + 1);
             }
         }
